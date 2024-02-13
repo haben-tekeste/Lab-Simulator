0fb012041938396f0b5c6d538c9caad3dfc8c647
haben-tekeste <haben.t00@gmail.com>
2024-02-13 17:46:39 +0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import SVG from "./components/SVG";
+import Setup from "./components/Setup";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <SVG />
+      <Setup />
     </div>
   );
 }
